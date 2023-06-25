8139d0b38be95acc0a0defd9cb05493d9047231b
dkulemin <blablamaildk@gmail.com>
2023-06-25 15:17:10 +0500

    class init and repr fixed

--- src/linear_models/linreg/linreg_1.py
+++ src/linear_models/linreg/linreg_1.py
@@ -1,12 +1,14 @@
 class MyLineReg():
-    def __init__(self, n_iter, learning_rate) -> None:
+
+    REPR_STR = '{class_name} class: n_iter={n_iter}, learning_rate={learning_rate}'
+
+    def __init__(self, n_iter=100, learning_rate=0.1) -> None:
         self.n_iter = n_iter
         self.learning_rate = learning_rate
 
     
     def __repr__(self) -> str:
-        representation_string = '{class_name} class: n_iter={n_iter}, learning_rate={learning_rate}'
-        return representation_string.format(
+        return self.REPR_STR.format(
             class_name=self.__class__.__name__,
             n_iter=self.n_iter,
             learning_rate=self.learning_rate
--- test/linear_models/linreg/test_linreg_1.py
+++ test/linear_models/linreg/test_linreg_1.py
@@ -1,4 +1,5 @@
 from mock import patch
+from mock.mock import PropertyMock
 import pytest
 
 from src.linear_models.linreg.linreg_1 import MyLineReg
@@ -9,26 +10,28 @@ def test_can_import_class():
 
 
 @pytest.mark.parametrize(
-        'n_iter, learning_rate, default_n_iter, default_lr',
+        'n_iter, learning_rate, etalon_n_iter, etalon_lr',
         [
             pytest.param(None, None, 100, 0.1, id='all_defaults'),
-            pytest.param(10, None, 100, 0.1, id='set_n_iter'),
-            pytest.param(None, 0.001, 100, 0.1, id='set_learning_rate'),
-            pytest.param(10, 0.001, 100, 0.1, id='set_all_params'),
+            pytest.param(10, None, 10, 0.1, id='set_n_iter'),
+            pytest.param(None, 0.001, 100, 0.001, id='set_learning_rate'),
+            pytest.param(10, 0.001, 10, 0.001, id='set_all_params'),
         ]
 )
-def test_class_initialization(n_iter, learning_rate, default_n_iter, default_lr):
-    linreg = MyLineReg(
-        n_iter if n_iter else default_n_iter,
-        learning_rate if learning_rate else default_lr
-    )
-    etalon_n_iter = n_iter if n_iter else default_n_iter
-    etalon_learning_rate = learning_rate if learning_rate else default_lr
+def test_class_initialization(n_iter, learning_rate, etalon_n_iter, etalon_lr):
+    if n_iter and learning_rate:
+        linreg = MyLineReg(n_iter, learning_rate)
+    elif n_iter:
+        linreg = MyLineReg(n_iter)
+    elif learning_rate:
+        linreg = MyLineReg(learning_rate=learning_rate)
+    else:
+        linreg = MyLineReg()
     assert linreg.n_iter == etalon_n_iter, (
         f'n_iter should be {etalon_n_iter}, but got {linreg.n_iter}'
     )
-    assert linreg.learning_rate == etalon_learning_rate, (
-        f'n_iter should be {etalon_learning_rate}, but got {linreg.learning_rate}'
+    assert linreg.learning_rate == etalon_lr, (
+        f'n_iter should be {etalon_lr}, but got {linreg.learning_rate}'
     )
 
 
@@ -41,19 +44,22 @@ def test_class_initialization(n_iter, learning_rate, default_n_iter, default_lr)
             pytest.param(10, 0.001, 100, 0.1, id='set_all_params'),
         ]
 )
-def test_class_representation(n_iter, learning_rate, default_n_iter, default_lr):
-    representation_string = 'MyLineReg class: n_iter={n_iter}, learning_rate={learning_rate}'
-    linreg = MyLineReg(
-        n_iter if n_iter else default_n_iter,
-        learning_rate if learning_rate else default_lr
-    )
-    print(linreg)
+@patch.object(MyLineReg, 'REPR_STR', new_callable=PropertyMock)
+def test_class_representation(mock_repr, n_iter, learning_rate, default_n_iter, default_lr):
     etalon_n_iter = n_iter if n_iter else default_n_iter
     etalon_learning_rate = learning_rate if learning_rate else default_lr
+    representation_string = '{class_name}: n_iter={n_iter}, lr={learning_rate}'
+    mock_repr.return_value = representation_string
+    linreg = MyLineReg(etalon_n_iter, etalon_learning_rate)
+    result = repr(linreg)
+    mock_repr.assert_called()
     etalon_representation = representation_string.format(
-        n_iter=etalon_n_iter, learning_rate=etalon_learning_rate
+        class_name = linreg.__class__.__name__,
+        n_iter=etalon_n_iter,
+        learning_rate=etalon_learning_rate
     )
-    assert linreg.__repr__() == etalon_representation, (
+
+    assert result == etalon_representation, (
         f'class representation should be {etalon_representation} '
         f'but got {linreg.__repr__()}'
     )
